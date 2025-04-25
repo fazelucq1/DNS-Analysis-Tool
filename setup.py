7ca99a2b7a6110c6fc8067259cbe77f2078fdf86
fazelucq <119872173+fazelucq1@users.noreply.github.com>
2025-04-25 19:36:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="dns_analyzer",
-    version="0.2.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         'dnspython>=2.0',
-        'reportlab>=3.6',
-        'Jinja2>=3.0',
-        'python-dotenv>=0.19'
+        'tqdm>=4.0',
+        'Jinja2>=3.0'
     ],
     entry_points={
         'console_scripts': [
-            'dnsanalyze = dns_analyzer.main:main'
+            'dnsanalyze = dns_analyzer.cli:main'
         ]
     },
     package_data={
         'dns_analyzer': ['templates/*.html']
     },
     include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
 )
